2aa7af359d943c98c785b9d7a3f02440698b5cfa
krupnovaa <krupnovaa@07-151-1.salut27.ru>
2018-03-31 09:44:37 +0300

    вторая редакция

---
+++ myCalcIpotec/public_html/newjavascript.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function changePercentRange() {
+    let percent = document.getElementById('percentRange').value;
+    document.getElementById('percentText').value = percent;
+    
+}
+
+function changePercentText() {
+    let percent = document.getElementById('percentText').value;
+    document.getElementById('percentRange').value = percent;
+}
+
+function nnn() {
+    
+}
+
